cb8f70321a455d19069b8f192594da72e6064002
Du_an_1 <nguyennghiep2007@gmail.com>
2020-07-20 10:38:32 +0700

    Lab 4

---
+++ app/src/main/java/com/example/buoi7/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.buoi7;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.tv);
+    }
+
+    public void getGPS(View view) {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            },999);
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 2000, 0,
+                new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        textView.setText(location.getLatitude()+" : " + location.getLongitude());
+                        Toast.makeText(MainActivity.this, location.getLatitude()+" : " + location.getLongitude(),Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                    }
+
+                    @Override
+                    public void onProviderEnabled(String provider) {
+
+                    }
+
+                    @Override
+                    public void onProviderDisabled(String provider) {
+
+                    }
+                });
+    }
+
+    public void internet(View view) {
+        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo isWifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo is3G = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (isWifi.isConnected()){
+            Toast.makeText(this,"Sử dụng wifi", Toast.LENGTH_SHORT).show();
+
+        }
+        if (is3G.isConnected()){
+            Toast.makeText(this,"Sử dụng 3G", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
